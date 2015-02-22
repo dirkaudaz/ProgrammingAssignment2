efd23c22f268a8bf7f3e72f0dbd4d67985fc3110
Sebastian Brugnoli <sbrugnol@gmail.com>
2015-02-22 19:48:07 -0300

    simple solution without error handling

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,65 @@
-## Put comments here that give an overall description of what your
-## functions do
+########################################################
+## Coursera - John Hopkins University - R Programming ##
+########################################################
+##
+## cachematrix.R defines a set of functions and helper
+## functions to efficiently calculate the inverse of a 
+## matrix. 
+##
+## Functions:
+## - cacheSolve(x, ...)
+##
+## Helper Functions:
+## - makeCacheMatrix(x)
+##
+########################################################
 
-## Write a short comment describing this function
-
+## makeCacheMatrix takes an object of type matrix and
+## binds to a private variable "m" that keeps the result 
+## of the operation performed over matrix "x" and four 
+## functions: 
+## - set: set matrix "X" to "Y" and resets "m" to NULL
+## - get: returns matrix "X"
+## - setOpResult: assigns to internal variable "m" the 
+##                value of argument "result"
+## - getOpResult: returns the value of "m"
+##
+## list command binds the functions to input matrix 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
-  set <- function(y) {
+  set <- function() {
     x <<- y
     m <<- NULL
   }
   get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  setOpResult <- function(result) m <<- result
+  getOpResult <- function() m
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setOpResult = setOpResult,
+       getOpResult = getOpResult)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve is an efficient implementation for
+## matrix inversion that makes use of helper function
+## makeCacheMatrix to cache solve operation over input
+## matrix, or retrieve the previously cached result in
+## case the same matrix is given as input.
+## To use this function matrices must be converted to
+## a "cacheable" matrix form through function 
+## makeCacheMatrix.
+##
+## Example: x_cache <- makeCacheMatrix(x)
+##            x_inv <- cacheSolve(x_cache)
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
-  m <- x$getmean()
+  m <- x$getOpResult()
   if(!is.null(m)) {
     message("getting cached data")
     return(m)
   }
   data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
+  m <- solve(data, ...)
+  x$setOpResult(m)
   m
 }
